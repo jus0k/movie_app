00fe06bd4234fe4b1c08f088372efaaf3a5c3651
jus0k <jutjd115@gmail.com>
2019-04-06 02:53:34 +0900

    Updates

---
+++ precache-manifest.f8cc1b16e9c1985d221503ae1412b48f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c6514544d436775abdc2",
+    "url": "/movie_app/static/js/runtime~main.175dda75.js"
+  },
+  {
+    "revision": "7fa1af2aca85b16aacbd",
+    "url": "/movie_app/static/js/main.38480ee2.chunk.js"
+  },
+  {
+    "revision": "74286a96ace76faac2ea",
+    "url": "/movie_app/static/js/2.5425dfe7.chunk.js"
+  },
+  {
+    "revision": "7fa1af2aca85b16aacbd",
+    "url": "/movie_app/static/css/main.72a464a7.chunk.css"
+  },
+  {
+    "revision": "95634aea78801b5d60fccee7faa594f7",
+    "url": "/movie_app/index.html"
+  }
+];
